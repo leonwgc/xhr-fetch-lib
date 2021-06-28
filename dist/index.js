55fa90470d67240c1f3d95d6da38f5036938cfce
leonwgc <wangguochao@zuifuli.com>
2021-06-28 22:00:03 +0800

    feat: init

---
+++ dist/index.js
@@ -0,0 +1,115 @@
+'use strict';
+
+var qs = require('qs');
+var JSONbig = require('json-bigint');
+
+function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+var qs__default = /*#__PURE__*/_interopDefaultLegacy(qs);
+var JSONbig__default = /*#__PURE__*/_interopDefaultLegacy(JSONbig);
+
+var JSONBig = JSONbig__default['default']({
+  storeAsString: true
+});
+
+function parseResponse(xhr) {
+  var result;
+
+  try {
+    result = JSONBig.parse(xhr.responseText);
+  } catch (e) {
+    result = xhr.responseText;
+  }
+
+  return result;
+}
+
+function hasContentType(headers) {
+  return Object.keys(headers).some(function (name) {
+    return name.toLowerCase() === 'content-type';
+  });
+} // application/x-www-form-urlencoded / application/json
+
+
+function setHeaders(xhr, headers) {
+  headers = headers || {};
+
+  if (!hasContentType(headers)) {
+    headers['Content-Type'] = 'application/json';
+  }
+
+  Object.keys(headers).forEach(function (name) {
+    if (headers[name]) {
+      xhr.setRequestHeader(name, headers[name]);
+    }
+  });
+}
+
+function objectToQueryString(data) {
+  return isObject(data) ? getQueryString(data) : data;
+}
+
+function isObject(data) {
+  return Object.prototype.toString.call(data) === '[object Object]';
+}
+
+function getQueryString(object) {
+  return qs__default['default'].stringify(object, {
+    indices: false
+  });
+}
+
+var fetch = function fetch(_ref) {
+  var _ref$method = _ref.method,
+      method = _ref$method === void 0 ? 'get' : _ref$method,
+      _ref$url = _ref.url,
+      url = _ref$url === void 0 ? '' : _ref$url,
+      _ref$data = _ref.data,
+      data = _ref$data === void 0 ? null : _ref$data,
+      _ref$headers = _ref.headers,
+      headers = _ref$headers === void 0 ? null : _ref$headers,
+      _ref$withCredentials = _ref.withCredentials,
+      withCredentials = _ref$withCredentials === void 0 ? true : _ref$withCredentials,
+      _ref$responseParser = _ref.responseParser,
+      responseParser = _ref$responseParser === void 0 ? parseResponse : _ref$responseParser;
+  var postData = '';
+
+  if (method === 'get') {
+    if (data) {
+      url += "?".concat(objectToQueryString(data));
+      data = null;
+    }
+  } else {
+    if (headers && headers['Content-Type'] === 'application/x-www-form-urlencoded') {
+      postData = objectToQueryString(data);
+    } else {
+      try {
+        postData = JSON.stringify(data);
+      } catch (ex) {}
+    }
+  }
+
+  return new Promise(function (resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.withCredentials = withCredentials;
+    setHeaders(xhr, headers);
+
+    xhr.onload = function () {
+      var res = responseParser(xhr);
+
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve(res);
+      } else {
+        reject({
+          xhr: xhr
+        });
+      }
+    };
+
+    xhr.onerror = reject;
+    xhr.send(postData);
+  });
+};
+
+module.exports = fetch;
---
+++ src/index.ts
@@ -0,0 +1,99 @@
+import qs from 'qs';
+// JSON.parse/stringify with bigints support
+import JSONbig from 'json-bigint';
+
+const JSONBig = JSONbig({ storeAsString: true });
+
+function parseResponse(xhr: XMLHttpRequest): unknown {
+  let result;
+
+  try {
+    result = JSONBig.parse(xhr.responseText);
+  } catch (e) {
+    result = xhr.responseText;
+  }
+  return result;
+}
+
+function hasContentType(headers: Record<string, string>) {
+  return Object.keys(headers).some((name) => {
+    return name.toLowerCase() === 'content-type';
+  });
+}
+
+// application/x-www-form-urlencoded / application/json
+function setHeaders(xhr: XMLHttpRequest, headers: Record<string, string>) {
+  headers = headers || {};
+  if (!hasContentType(headers)) {
+    headers['Content-Type'] = 'application/json';
+  }
+  Object.keys(headers).forEach((name) => {
+    if (headers[name]) {
+      xhr.setRequestHeader(name, headers[name]);
+    }
+  });
+}
+function objectToQueryString(data: Record<string, unknown> | string) {
+  return isObject(data) ? getQueryString(<Record<string, unknown>>data) : data;
+}
+
+function isObject(data) {
+  return Object.prototype.toString.call(data) === '[object Object]';
+}
+
+function getQueryString(object: Record<string, unknown>) {
+  return qs.stringify(object, { indices: false });
+}
+
+export type Options = {
+  method?: 'get' | 'post' | 'put' | 'delete' | 'head';
+  url: string;
+  data?: Record<string, unknown> | null;
+  headers?: Record<string, string>;
+  withCredentials?: boolean;
+  responseParser?: (xhr: XMLHttpRequest) => unknown;
+};
+
+const fetch = ({
+  method = 'get',
+  url = '',
+  data = null,
+  headers = null,
+  withCredentials = true,
+  responseParser = parseResponse,
+}: Options): Promise<unknown> => {
+  let postData: string | Record<string, unknown> = '';
+  if (method === 'get') {
+    if (data) {
+      url += `?${objectToQueryString(data)}`;
+      data = null;
+    }
+  } else {
+    if (headers && headers['Content-Type'] === 'application/x-www-form-urlencoded') {
+      postData = objectToQueryString(data);
+    } else {
+      try {
+        postData = JSON.stringify(data);
+      } catch (ex) {}
+    }
+  }
+
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.withCredentials = withCredentials;
+    setHeaders(xhr, headers);
+    xhr.onload = () => {
+      const res = responseParser(xhr);
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve(res);
+      } else {
+        reject({ xhr });
+      }
+    };
+    xhr.onerror = reject;
+    xhr.send(postData as string);
+  });
+};
+
+export default fetch;
